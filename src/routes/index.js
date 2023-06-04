968b18ffc1509c4aafb02222d3bff82d75b83b2d
Prathmeshdeshmukh <prathmeshd60@gmail.com>
2023-06-04 18:50:37 +0530

    protected comments by auth passport

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,5 +15,5 @@ router.get('/tweets/:id', getTweet );
 
 router.post('/likes/toggle', toggleLike);
 
-router.post('/comments', createComment);
+router.post('/comments', authenticate, createComment);
 export default router;
